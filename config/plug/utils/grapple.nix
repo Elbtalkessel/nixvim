7df6749c00b5bd5075399c018791f8c872e8c0e1
Elbtalkessel <rtfsc@pm.me>
2024-06-02 19:43:05 +0200

    simplify grapple bindings

--- config/plug/utils/grapple.nix
+++ config/plug/utils/grapple.nix
@@ -17,4 +17,69 @@
       scope = "git_branch",
     })
   '';
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>j";
+      action = "+j";
+      options = {desc = "Jump (grapple)";};
+    }
+
+    {
+      mode = "n";
+      key = "<leader>jt";
+      action = "<CMD>Grapple toggle<CR>";
+      options = {desc = "Tag a file";};
+    }
+
+    {
+      mode = "n";
+      key = "<leader>jm";
+      action = "<CMD>Grapple toggle_tags<CR>";
+      options = {desc = "Toggle tag menu";};
+    }
+
+    {
+      mode = "n";
+      key = "<leader>jl";
+      action = "<CMD>Grapple cycle_tags next<CR>";
+      options = {desc = "Go to next tag";};
+    }
+
+    {
+      mode = "n";
+      key = "<leader>jh";
+      action = "<CMD>Grapple cycle_tags prev<CR>";
+      options = {desc = "Go to previous tag";};
+    }
+
+    {
+      mode = "n";
+      key = "<leader>j1";
+      action = "<CMD>Grapple select index=1<CR>";
+      options = {desc = "Go to tag 1";};
+    }
+
+    {
+      mode = "n";
+      key = "<leader>j2";
+      action = "<CMD>Grapple select index=2<CR>";
+      options = {desc = "Go to tag 2";};
+    }
+
+    {
+      mode = "n";
+      key = "<leader>j3";
+      action = "<CMD>Grapple select index=3<CR>";
+      options = {desc = "Go to tag 3";};
+    }
+
+    {
+      mode = "n";
+      key = "<leader>j4";
+      action = "<CMD>Grapple select index=4<CR>";
+      options = {desc = "Go to tag 4";};
+    }
+  ];
 }
