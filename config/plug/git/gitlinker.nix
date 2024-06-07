3bd40b4d4f0f0d46210ab14587fe7c40ed7c835b
Elbtalkessel <rtfsc@pm.me>
2024-06-07 18:45:34 +0200

    add callback to gitea

--- config/plug/git/gitlinker.nix
+++ config/plug/git/gitlinker.nix
@@ -3,7 +3,7 @@
     enable = true;
     callbacks = {
       "github.com" = "get_github_type_url";
-      "gitlab.dnm.radiofrance.fr" = "get_github_type_url";
+      "gitea.42cc.co" = "get_github_type_url";
     };
   };
 }
