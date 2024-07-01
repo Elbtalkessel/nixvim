add36b021e85139af709d8aa335311493e0d1622
Elbtalkessel <rtfsc@pm.me>
2024-07-01 17:52:45 +0200

    Mon 01 Jul 2024 17:52:45 CEST

--- config/plug/ui/bufferline.nix
+++ config/plug/ui/bufferline.nix
@@ -66,24 +66,6 @@ in {
   keymaps = [
     {
       mode = "n";
-      key = "<Tab>";
-      action = "<cmd>BufferLineCycleNext<cr>";
-      options = {
-        desc = "Cycle to next buffer";
-      };
-    }
-
-    {
-      mode = "n";
-      key = "<S-Tab>";
-      action = "<cmd>BufferLineCyclePrev<cr>";
-      options = {
-        desc = "Cycle to previous buffer";
-      };
-    }
-
-    {
-      mode = "n";
       key = "<S-l>";
       action = "<cmd>BufferLineCycleNext<cr>";
       options = {
