fb024cc348ec411874e8e965cf89b27d6b8b7d91
Elbtalkessel <rtfsc@pm.me>
2025-02-13 12:41:00 +0100

    Thu 13 Feb 2025 12:41:00 CET

--- modules/nixvim/options.nix
+++ modules/nixvim/options.nix
@@ -35,6 +35,17 @@
     bufferline-style = lib.mkOption {
       default = false;
       type = lib.types.bool;
+      description = "If enabled bufferline will use custom style, by default it tries to match current theme automatically.";
+    };
+    spelllang = lib.mkOption {
+      default = null;
+      type = lib.types.nullOr lib.types.str;
+      description = "Disabled by default. To enable use valid attribue of nvim's spelllang, i.e. 'en,uk'.";
+    };
+    spellcheck = lib.mkOption {
+      default = [ ];
+      type = lib.type.listOf lib.types.str;
+      description = "List of file patterns to enable spellcheck for. Without setting the `spellanag` option, this one does nothing.";
     };
   };
 }
