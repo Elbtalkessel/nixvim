28e390078c03745f49ce9ab5c7df18fa091bc811
Elbtalkessel <rtfsc@pm.me>
2026-01-08 10:29:10 +0100

    Thu 08 Jan 2026 10:29:10 CET

---
+++ config/plugins/everything/default.nix
@@ -0,0 +1,11 @@
+# Plugins that doesn't not require configuration, only `enable = true`.
+{
+  plugins = {
+    lsp-lines.enable = true; # In-line diagnostic messages.
+    lz-n.enable = true; # Lazy plugin loading.
+    noice.enable = true; # Messages, commands, popups.
+    quicker.enable = true; # Mass search / replace.
+    nvim-ufo.enable = true; # Code floding.
+    colorizer.enable = true; # Color highlight.
+  };
+}
--- config/plugins/gitlinker/default.nix
+++
@@ -1,5 +0,0 @@
-{
-  plugins.gitlinker = {
-    enable = false;
-  };
-}
